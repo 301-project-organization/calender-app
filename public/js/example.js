315e8240d75811c0900eb0fe5bd9524b9e6559ca
mrsmarah <marahabdallah91@gmail.com>
2020-04-22 12:24:23 +0300

    css final touches

--- public/js/example.js
+++ public/js/example.js
@@ -1,6 +1,7 @@
 console.log("menu");
 
 $(document).ready(function(){
+    console.log("hello");
     /*  hide-show mobile menu  */
     $("#menu_icon").click(function(){
         $("#nav_menu").toggleClass("show_menu");
